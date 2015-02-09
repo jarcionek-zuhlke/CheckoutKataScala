f1593f21865f06fd3d5b6298e1532c8600c24054
Jaroslaw Pawlak <jaroslaw.pawlak@zuhlke.com>
2015-02-09 11:46:19 +0000

    implemented a lot of stuff for offers

--- src/main/java/checkoutscala/CheckoutSystem.scala
+++ src/main/java/checkoutscala/CheckoutSystem.scala
@@ -6,17 +6,69 @@ object CheckoutSystem {
     type Price = Int
 
     def checkout(products: Item*) = {
+        val offerMap: Map[Offer, List[Item]] = createMap(products)
+
         var totalPrice = 0
-        for (item <- products) {
-            totalPrice += priceOf(item)
+
+        for ((offer: Offer, items: List[Item]) <- offerMap) {
+            val prices: List[Price] = items.map(item => priceOf(item))
+
+            var paidCount = 0
+            var gotFreeCount = 0
+
+            for (price <- prices) {
+                if (paidCount == offer.payFor && gotFreeCount < offer.getFree) {
+                    gotFreeCount += 1
+                    // get current item for free
+                    if (gotFreeCount == offer.getFree) {
+                        paidCount = 0
+                        gotFreeCount = 0
+                    }
+                } else {
+                    totalPrice += price
+                    paidCount += 1
+                }
+            }
         }
+
         totalPrice
     }
 
+    private def createMap(products: Seq[Item]): Map[Offer, List[Item]] = {
+        var offers: Set[Offer] = Set()
+
+        for (item <- products) {
+            offers = offers.+(offerFor(item))
+        }
+
+        var offerMap: Map[Offer,List[Item]] = Map()
+
+        for (offer <- offers) {
+            var items: List[Item] = List()
+            for (item <- products) {
+                if (offerFor(item) == offer) {
+                    items = items :+ item
+                }
+            }
+            offerMap = offerMap.+((offer, items))
+        }
+
+        offerMap
+    }
+    
     private def priceOf(product: Item): Price = {
         product match {
             case "Apple" => 60
             case "Orange" => 25
+        }
+    }
+
+    private case class Offer(payFor: Int, getFree: Int)
+
+    private def offerFor(product: Item): Offer = {
+        product match {
+            case "Apple" => Offer(1, 1)
+            case _ => Offer(1, 0)
         }
     }
 
--- src/test/java/checkoutscala/CheckoutSystemTest.scala
+++ src/test/java/checkoutscala/CheckoutSystemTest.scala
@@ -4,18 +4,25 @@ import org.scalatest._
 
 class CheckoutSystemTest extends FlatSpec with Matchers {
 
+    val APPLE_PRICE: Int = 60
+    val ORANGE_PRICE: Int = 25
+
     val checkoutSystem = CheckoutSystem
 
     "single apple" should "cost 60p" in {
-        checkoutSystem.checkout("Apple") should be (60)
+        checkoutSystem.checkout("Apple") should be (APPLE_PRICE)
     }
 
     "single orange" should "cost 25p" in {
-        checkoutSystem.checkout("Orange") should be (25)
+        checkoutSystem.checkout("Orange") should be (ORANGE_PRICE)
     }
 
-    "3 apples and orange" should "cost £2.05" in {
-        checkoutSystem.checkout("Apple", "Apple", "Orange", "Apple") should be (205)
+    "multiple items without offers" should "have total cost equal to sum of their individual prices" in {
+        checkoutSystem.checkout("Apple", "Orange", "Orange") should be (APPLE_PRICE + 2 * ORANGE_PRICE)
+    }
+
+    "apple" should "be in offer \"buy one get one free\"" in {
+        checkoutSystem.checkout("Apple", "Apple") should be (APPLE_PRICE)
     }
 
 }
